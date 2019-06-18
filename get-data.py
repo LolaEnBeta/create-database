ebd49b5550ae5306eec407e0efa8c8a730a366d7
LolaEnBeta <lolitaenbeta@gmail.com>
2019-06-18 17:47:35 +0200

    Execute query

--- get-data.py
+++ get-data.py
@@ -5,3 +5,11 @@ conn = sqlite3.connect("***/***.db")
 query = conn.cursor()
 
 sql = "SELECT * FROM users"
+
+#Execute query
+if (query.execute(sql)):
+    filas = query.fetchall()
+    for fila in filas:
+        print(fila[0], fila[1], fila[2])
+else:
+    print("An error has ocurred")
